8b70c5e621c3ba281294f3d013f2b2fe0261da72
lprates <lprates23@hotmail.com>
2019-01-26 11:57:56 +0000

    modified handler to receive json as input

--- fibonacci-java/fibonacci-java/src/main/java/com/openfaas/function/Handler.java
+++ fibonacci-java/fibonacci-java/src/main/java/com/openfaas/function/Handler.java
@@ -5,11 +5,21 @@ import com.openfaas.model.IResponse;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
 
+import com.google.gson.*;
+
 public class Handler implements com.openfaas.model.IHandler {
 
     public IResponse Handle(IRequest req) {
         Response res = new Response();
-	    res.setBody("Hello, world!");
+	    
+	    Gson gson = new Gson();
+	    
+	    
+	    FibonacciInput fibonacciInput = gson.fromJson(req.getBody(), FibonacciInput.class);
+	    
+	    String body = Integer.toString(fibonacci(fibonacciInput.getFibonacciInput()));
+	    
+	    res.setBody("Fibonacci value of "+fibonacciInput.getFibonacciInput()+" is "+body);
 
 	    return res;
     }
@@ -23,4 +33,13 @@ public class Handler implements com.openfaas.model.IHandler {
 		  return fibonacci(n - 1) + fibonacci(n - 2);
 		}
 	}
+	
+}
+
+class FibonacciInput{
+		private int n=1;
+		
+		public int getFibonacciInput(){
+			return this.n;
+		}
 }
