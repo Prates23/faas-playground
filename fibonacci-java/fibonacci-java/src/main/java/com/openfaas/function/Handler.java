9057e94e911d7deb361f714e1be3bcbe5200141a
Luis Prates <luis.prates@celfocus.com>
2019-01-26 12:24:20 +0000

    modified input to be a randow number between 1 and 10

--- fibonacci-java/fibonacci-java/src/main/java/com/openfaas/function/Handler.java
+++ fibonacci-java/fibonacci-java/src/main/java/com/openfaas/function/Handler.java
@@ -11,37 +11,32 @@ public class Handler implements com.openfaas.model.IHandler {
 
     public IResponse Handle(IRequest req) {
         Response res = new Response();
-	    
-	    Gson gson = new Gson();
-	    
-	    
-	    FibonacciInput fibonacciInput = gson.fromJson(req.getBody(), FibonacciInput.class);
 
-	    int inputFibonacci = Strings
-	    
-	    String body = Integer.toString(fibonacci(fibonacciInput.getFibonacciInput()));
-	    
-	    res.setBody("Fibonacci value of "+fibonacciInput.getFibonacciInput()+" is "+body);
 
-	    return res;
+        int input = randomWithRange(1,10);
+
+        String body = Integer.toString(fibonacci(input));
+
+        res.setBody("Fibonacci value of " + input + " is " + body);
+
+        return res;
     }
-    
-	public int fibonacci(int n)  {
-		if(n == 0){
-			return 0;
-		}else if(n == 1){
-		  return 1;
-	    }else{
-		  return fibonacci(n - 1) + fibonacci(n - 2);
-		}
-	}
-	
+
+    public int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    int randomWithRange(int min, int max){
+        int range = (max - min) + 1;
+        return (int)(Math.random() * range) + min;
+    }
+
 }
 
-class FibonacciInput{
-		private String input="1";
-		
-		public int getFibonacciInput(){
-			return this.input;
-		}
-}
+
